579ff968fec56db7c69a395cd12f0824accd30e2
Sebastian-Balint <sebastian.balint02@gmail.com>
2024-03-22 09:25:29 +0100

    ADD carosello da fixare

--- js/script.js
+++ js/script.js
@@ -43,3 +43,51 @@ const images = [
     text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
   },
 ];
+
+let activeImg = 0;
+
+const carouselEl = document.querySelector(".mainColumn");
+const thumbnailsEl = document.querySelector(".thumbnails");
+const beforeButton = document.getElementById("previous");
+const afterButton = document.getElementById("after");
+
+createFocusImage(images, carouselEl, activeImg);
+createThumbnails(images, thumbnailsEl);
+
+beforeButton.addEventListener("click", () => {
+  activeImg == 0 ? (activeImg = images.length) : 0;
+
+  activeImg -= 1;
+
+  createFocusImage(images, carouselEl, activeImg);
+});
+
+afterButton.addEventListener("click", () => {
+  activeImg == images.length - 1 ? (activeImg = -1) : 0;
+
+  activeImg += 1;
+
+  createFocusImage(images, carouselEl, activeImg);
+});
+
+// Functions //
+
+function createFocusImage(images, output, activeImg) {
+  output.innerHTML = `
+    <article>
+        <img src="${images[activeImg].image}" alt="${images[activeImg].title}'s photo">
+        <div class="description">
+            <h2>${images[activeImg].title}</h2>
+            <p>${images[activeImg].text}</p>
+        </div>
+    </article>`;
+}
+
+function createThumbnails(images, output) {
+  images.forEach((element) => {
+    output.innerHTML += `
+        <div class="thumbnail">
+            <img src="${element.image}" alt="">
+        </div>`;
+  });
+}
